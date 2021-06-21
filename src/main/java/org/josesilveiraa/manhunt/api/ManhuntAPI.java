2661d6272e8ea5d38e6de7b9823c01962ea2adee
Josesilveiraa <josesilveiraa@protonmail.com>
2021-06-21 13:14:52 -0300

    refactor

--- src/main/java/org/josesilveiraa/manhunt/api/ManhuntAPI.java
+++ src/main/java/org/josesilveiraa/manhunt/api/ManhuntAPI.java
@@ -1,16 +1,19 @@
 package org.josesilveiraa.manhunt.api;
 
+import org.jetbrains.annotations.NotNull;
 import org.josesilveiraa.manhunt.Main;
 import org.josesilveiraa.manhunt.manager.GameManager;
 import org.josesilveiraa.manhunt.object.Game;
 
-public class ManhuntAPI {
+public final class ManhuntAPI {
 
-    public static Game getGame() {
+    @NotNull
+    public final Game getGame() {
         return Main.getGame();
     }
 
-    public static GameManager getGameManager() {
+    @NotNull
+    public final GameManager getGameManager() {
         return Main.getGameManager();
     }
 
